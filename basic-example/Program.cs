0ea6d78903154ea8ebd30c9e4d11f0b1a4f166b9
Rene Cincura <rene@cincura.eu>
2022-04-15 10:09:27 +0200

    refactor: Change example according to API improvements

--- basic-example/Program.cs
+++ basic-example/Program.cs
@@ -1,55 +1,52 @@
 ﻿// Server connection
-var cord = new CordClient(Endpoint.Sigma);
+var cord = new CordClient();
 
-var tcsReady = new TaskCompletionSource();
-cord.Ready += async () => tcsReady.SetResult();
-
-// Login by email password
+// Login by email/password and wait until Ready is received
 await cord.LoginAsync("example@examplecom", "password");
 
-// Wait 'till client is ready
-await tcsReady.Task;
 
+// List all joined rooms
+foreach (var room in cord.Rooms) {
+    Console.WriteLine($"Cord is ready {room.Id} name {room.Name}");
+}
 
-cord.Ready += async () => {
-    // All joined rooms
-    foreach (var room in cord.Rooms) {
-        Console.WriteLine($"Cord is ready {room.Id} name {room.Name}");
-    }
+// List all available users
+foreach (var user in cord.Users) {
+    Console.WriteLine($"Cord is ready {user.Id} name {user.Name}");
+}
 
-    // All available users
-    foreach (var user in cord.Users) {
-        Console.WriteLine($"Cord is ready {user.Id} name {user.Name}");
-    }
+// Enter room by URI link
+await cord.EnterRoom("3ir5ug379kp");
 
-    // Enter room by URI link
-    await cord.EnterRoom("3ir5ug379kp");
-};
 
 cord.MessageCreated += async (room, message) => {
     // If user is not member of the room anymore, his basic info is provided in <Author>
     var user = message.Member?.User ?? message.Author;
     var text = message.Text?.ToLower() ?? string.Empty;
 
-    if (text == "hey") {
-        await room.SendMessage($"Hello, {user.Name}!");
-    } else if (text == "how are you?") {
-        await room.SendMessage("I'm Fine, thanks! And you?");
-    } else if (text == "fine") {
-        await room.SendMessage("Cool");
-    }
+    switch (text) {
+        case "hey":
+            await room.SendMessage($"Hello, {user.Name}!");
+            break;
+        case "how are you?":
+            await room.SendMessage("I'm Fine, thanks! And you?");
+            break;
+        case "fine":
+            await room.SendMessage("Cool");
+            break;
+        case "/clear all": {
+            Console.WriteLine($"Deleting all messages {room.Messages?.Count}");
 
+            foreach (var msg in room.Messages!) {
+                await msg.Delete();
+            }
+            break;
+        }
+    }
+    
     if (text.Contains("fuck")) {
         await message.Delete();
         await room.SendMessage("Don't swar!");
-    }
-
-    if (text == "/clear all") {
-        Console.WriteLine($"Deleting all messages {room.Messages?.Count}");
-
-        foreach (var msg in room.Messages!) {
-            await msg.Delete();
-        }
     }
 };
 
